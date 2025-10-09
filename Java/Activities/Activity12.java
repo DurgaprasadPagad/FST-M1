f8e22578b43bf893d8364a7b30750c1e476ca411
DurgaprasadPagad <durgapagad84@gmail.com>
2025-10-09 10:53:34 +0530

    Activity12 - Lambda assignment

---
+++ Java/Activities/Activity12.java
@@ -0,0 +1,19 @@
+
+public class Activity12 {
+	
+	interface Addable{
+		int add(int num1, int num2);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Addable ad1=(num1, num2) -> (num1 + num2);
+		System.out.println(ad1.add(20, 30));
+		
+		Addable ad2= (int num1, int num2) -> {
+			return (num1 + num2);
+		};
+		System.out.println(ad2.add(40, 50));
+	}
+	
+}
